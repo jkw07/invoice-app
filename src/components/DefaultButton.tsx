0a7d293f1a61788d4cab3c0cdb19f1e277b5a30c
Justyna W <jkwodecka@gmail.com>
2025-03-18 05:41:04 +0100

    change default button

--- src/components/DefaultButton.tsx
+++ src/components/DefaultButton.tsx
@@ -3,28 +3,20 @@ import "../styles/buttons.scss";
 interface DefaultButtonProps {
   text?: string;
   icon?: React.ReactElement;
-  type?: "button" | "submit" | "reset" | "return";
+  type?: "submit" | "reset" | "button" | undefined;
+  className?: "default-button" | "reset-buton" | "return-button";
   onClick?: () => void;
 }
 
 export const DefaultButton = ({
   text = "",
   icon,
+  className = "default-button",
   type = "button",
   onClick,
 }: DefaultButtonProps) => {
-  const buttonClass =
-    type === "reset"
-      ? "reset-button"
-      : type === "return"
-      ? "return-button"
-      : "default-button";
   return (
-    <button
-      type={type === "return" ? "button" : type}
-      className={buttonClass}
-      onClick={onClick}
-    >
+    <button type={type} className={className} onClick={onClick}>
       {icon}
       {text}
     </button>
